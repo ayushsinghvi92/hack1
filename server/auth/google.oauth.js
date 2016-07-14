73a9da1207b0ff4a4b7e9fddbe045a82782e8453
mosane <ayush.singhvi@nyu.edu>
2016-07-14 10:54:09 -0400

    fixed a major security flaw

---
+++ server/auth/google.oauth.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var User = require('../api/users/user.model');
+var google = require('../../secrets/google.auth')
+
+module.exports = new GoogleStrategy({
+  clientID: google.clientID,
+  clientSecret: google.clientSecret,
+  callbackURL: google.callbackURL
+}, function (token, refreshToken,  profile, triggerSerializationOfUser) {
+  // this only runs when somebody logs in through google
+  User.findOrCreate({
+    where: {googleId: profile.id},
+    defaults: {
+      email: profile.emails[0].value,
+      name: profile.displayName,
+      photo: profile.photos[0].value
+    }
+  })
+  .spread(function (user) {
+    triggerSerializationOfUser(null, user);
+  })
+  .catch(triggerSerializationOfUser);
+});
